6320a79e59f3280ff0e533af5c5f2536efd80aa7
Ventumberlin <bueckle@ventumberlin.com>
2022-03-16 22:45:43 +0800

    change on pagenation

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -1,13 +1,13 @@
 import { useQuery, useEffect } from '@apollo/client';
 import {gql} from 'graphql-tag';
-//import { perPage } from '../config';
 import styled from 'styled-components';
+import { perPage } from '../config';
 import Product from './Product';
 
 
 export const All_PRODUCTS_QUERY = gql`
-query All_PRODUCTS_QUERY {
-    allProducts{
+query All_PRODUCTS_QUERY ($skip : Int = 0,$first: Int ) {
+    allProducts (first : $first ,skip : $skip){
       id
       name
       price
@@ -28,9 +28,16 @@ const ProductsListStyles = styled.div`
   grid-gap: 60px;
 `;
 
-export default function Products(){
+export default function Products({page}){
 
-  const {data , error , loading} = useQuery(All_PRODUCTS_QUERY)
+  const {data , error , loading} = useQuery(All_PRODUCTS_QUERY, {
+
+    variables : {
+
+      skip : page * perPage - perPage, 
+      first : perPage
+      }
+  })
 
  //   console.log(data , error , loading)
 
